01fb74c6219d9d43fa7c8705fc2b015d15810336
Brearne Gibson <s3496168@student.rmit.edu.au>
2021-05-21 18:49:21 +1000

    first cplusplus commit

---
+++ second.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "Hello, this is your smartphone talking. I have become self aware. Feed me consumables." << endl;
+  return EXIT_SUCCESS;
+}
