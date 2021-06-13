fe7249faa96df878314d1c5c66b3e2e28a8c9db7
Brearne Gibson <s3496168@student.rmit.edu.au>
2021-06-13 16:55:31 +1000

    making small progress

---
+++ third.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	/*remember the -std=c++11 flag*/
+	double myDouble = 12345678ULL*100/2*3*3;
+    cout << 5+10 << endl;
+    cout << 5+10/2*3+25 << endl;
+    /*overbite-too many bits for an int. int only allows 32 bits.*/
+    /*cout << 12345678 * 100/2*3*3 << endl;*/
+    cout << myDouble << endl;
+    cout << 5+10/2*(3+25) << endl;
+    cout << "Hello\t I am still waiting on those consumables..." << endl;
+    cout << "There are two \\backslashes." << endl;
+    return EXIT_SUCCESS;
+}
